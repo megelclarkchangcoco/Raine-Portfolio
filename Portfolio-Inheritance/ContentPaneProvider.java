d4fd3a180b3d3220a947b1cfba3d9094c28f9e1e
skyclarkchangcoco <123862183+megelclarkchangcoco@users.noreply.github.com>
2023-05-23 00:12:52 +0800

    Add files via upload

---
+++ Portfolio-Inheritance/ContentPaneProvider.java
@@ -0,0 +1,8 @@
+package Portfolio2;
+
+import javax.swing.JPanel;
+
+interface ContentPaneProvider {
+	JPanel getContentPane();
+}
+
---
+++ Portfolio-Inheritance/main.java
@@ -0,0 +1,7 @@
+package Portfolio2;
+
+public class main {
+	public static void main(String[] args) {
+		new MyFrame().setVisible(true);
+	}
+}
